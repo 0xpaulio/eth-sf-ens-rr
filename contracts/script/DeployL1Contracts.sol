dfe51bcca07bbfd0ca384768bd0b604bdfb79859
paulio.eth <paulio.eth@gmail.com>
2022-11-05 17:03:48 -0700

    deployment scripts

--- contracts/script/DeployL1Contracts.sol
+++ contracts/script/DeployL1Contracts.sol
@@ -3,10 +3,31 @@ pragma solidity ^0.8.15;
 
 import "forge-std/Script.sol";
 
+import {L1ENSRegistry} from "src/l1/L1ENSRegistry.sol";
+
 contract CounterScript is Script {
-    function setUp() public {}
+    L1ENSRegistry l1Registry;
+
+    uint64 constant private  _optimismChainId = 10;
+    address constant private _l2RegistrarAddress = address(0x0);
+
+    address constant private _goerliOVMAddressManager = address(0xa6f73589243a6A7a9023b1Fa0651b1d89c177111);
+
+    string[] _gatewayUrls;
+
+    function setUp() public {
+        _gatewayUrls = new string[](1);
+        _gatewayUrls[0] = "http://localhost:3000/";
+    }
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+
+        // Deploy the L1 Registry
+        l1Registry = new L1ENSRegistry(
+            _optimismChainId, _l2RegistrarAddress,
+            _goerliOVMAddressManager,
+            _gatewayUrls
+        );
     }
 }
--- contracts/script/DeployL2Contracts.sol
+++ contracts/script/DeployL2Contracts.sol
@@ -3,10 +3,67 @@ pragma solidity ^0.8.15;
 
 import "forge-std/Script.sol";
 
+import {L2ENSRegistry} from "src/l2/L2ENSRegistry.sol";
+
 contract CounterScript is Script {
-    function setUp() public {}
+    L2ENSRegistry l2Registry;
+
+    address _owner = 0x84C970BFcD59a0e98eC6f13Cbdf24AA1a741f033;
+    address _goerliENSResolver = 0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e;    
+
+    string[] _gatewayUrls;
+
+    function setUp() public {
+        _gatewayUrls = new string[](1);
+        _gatewayUrls[0] = "http://localhost:3000/";
+    }
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+
+        // Deploy the L2 Registry
+        l2Registry = new L2ENSRegistry();
+
+        // Register the eth domain
+        l2Registry.setSubnodeRecord(
+            0x0,
+            keccak256("eth"),
+            _owner,
+            _goerliENSResolver,
+            64
+        );
+
+        bytes32 _nullRoot;
+        bytes32 _ethRoot = keccak256(
+            abi.encodePacked(
+                _nullRoot, 
+                keccak256("eth")
+            )
+        );
+
+        // Register the paulio.eth subdomain
+        l2Registry.setSubnodeRecord(
+            _ethRoot,
+            keccak256("paulio"),
+            _owner,
+            _goerliENSResolver,
+            64
+        );
+
+        bytes32 _paulioRoot = keccak256(
+            abi.encodePacked(
+                _ethRoot, 
+                keccak256("paulio")
+            )
+        );
+
+        // Register the lmeow.paulio.eth subdomain
+        l2Registry.setSubnodeRecord(
+            _paulioRoot,
+            keccak256("lmeow"),
+            _owner,
+            _goerliENSResolver,
+            64
+        );
     }
 }
